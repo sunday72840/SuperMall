aa2a57656bfcae803d7c33cbc870752dacacea12
sunday72840 <c58580@163.com>
2021-10-21 22:56:20 +0800

    updata code

---
+++ src/components/content/tabControl/tabControl.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tab-control">
+        <div v-for="(item,index) in titles" :key="index">
+            <span>{{item}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'tabControl',
+        props:{
+            titles:{
+                type:Array,
+                default(){
+                    return []
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .tab-control {
+        display: flex;
+    }
+    .tab-control-item {
+        flex: 1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,43 @@
         <nav-bar class="home-nav"><div slot="center" class="text-name">购物街</div></nav-bar>
         <home-swiper :banners="banners"></home-swiper>
         <home-recommend-view :recommends="recommends"/>
+        <feature-view></feature-view>
+
+        <tab-control :titles="['流行','新款','精选']"></tab-control>
+        <ul>
+            <li>列表1</li>
+            <li>列表2</li>
+            <li>列表3</li>
+            <li>列表4</li>
+            <li>列表5</li>
+            <li>列表6</li>
+            <li>列表7</li>
+            <li>列表8</li>
+            <li>列表9</li>
+            <li>列表10</li>
+            <li>列表11</li>
+            <li>列表12</li>
+            <li>列表13</li>
+            <li>列表14</li>
+            <li>列表15</li>
+            <li>列表16</li>
+            <li>列表17</li>
+            <li>列表18</li>
+            <li>列表19</li>
+            <li>列表20</li>
+        </ul>
     </div>
 </template>
 
 <script>
 
-import NavBar from 'components/common/navbar/NavBar'
+
 import HomeSwiper from './childComps/HomeSwiper'
 import HomeRecommendView from './childComps/HomeRecommendView'
+import FeatureView from './childComps/FeatureView.vue'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/tabControl'
 
 import {getHomeMultidata} from 'network/home.js'
 
@@ -20,7 +49,10 @@ name:'Home',
 components:{
     NavBar,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    FeatureView,
+    TabControl
+    
 },
 
 // 把下面获取的数据保存起来
@@ -43,8 +75,18 @@ created () {
 </script>
 
 <style>
+    #home {
+        padding-top: 44px;
+    }
     .home-nav {
         background-color: var(--color-tint);
+        color: #fff;
+
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 9;
     }
 
     .text-name {
